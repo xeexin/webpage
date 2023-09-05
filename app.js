76f6e604c72fc204c80a1f32233462d95e14bec9
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-06 00:14:23 +0900

    classList

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
+  const clickClass = "clicked";
 
-  if (currentColor === "pink") {
-    newColor = "green";
-  } else {
-    newColor = "pink";
-  }
+  //   if (h1.classList.contains(clickClass)) {
+  //     h1.classList.remove(clickClass);
+  //   } else {
+  //     h1.classList.add(clickClass);
+  //   }
 
-  h1.style.color = newColor;
+  h1.classList.toggle(clickClass);
 }
 
 h1.addEventListener("click", handleTitleClick);
