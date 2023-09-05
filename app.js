c15f9533895790045e117c5c8768ad2444c8088a
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-05 23:56:31 +0900

    css in js

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 const h1 = document.querySelector(".hello h1");
+
 function handleTitleClick() {
-  console.log("clicked");
-}
+  const currentColor = h1.style.color;
+  let newColor;
 
-function handleTitleMouse() {
-  h1.style.color = "purple";
-  h1.innerText = "Mouse is here";
-}
+  if (currentColor === "pink") {
+    newColor = "green";
+  } else {
+    newColor = "pink";
+  }
 
-function handleMouseLeave() {
-  h1.innerText = "Mouse  is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "pink";
-}
-
-function handleH1Copy() {
-  alert("YOU 🚨");
-}
-function handleWindowOffline() {
-  alert("NO WIFI");
-}
-function handleOnline() {
-  alert("Connected");
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleTitleMouse);
-h1.addEventListener("mouseleave", handleMouseLeave);
-h1.addEventListener("copy", handleH1Copy);
-
-window.addEventListener("resize", handleWindowResize);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleOnline);
