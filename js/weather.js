2d47dafeafa851f967a337ec17935c864a4700ba
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-08 20:39:50 +0900

    weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "7d37eb150b99bc097591d527fd80c392";
+
+function onGeoOk(position) {
+  const lati = position.coords.latitude;
+  const longi = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
